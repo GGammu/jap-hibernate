73cfabe06430aeaba335225a948bb57e4aab6dcb
Young Hwang <ykcul02@gmail.com>
2021-10-11 18:47:37 +0900

    fix: 값 타입 예제 적용

--- src/main/java/io/ggammu/study/jpa/Address.java
+++ src/main/java/io/ggammu/study/jpa/Address.java
@@ -1,19 +1,22 @@
 package io.ggammu.study.jpa;
 
 import java.util.Objects;
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import lombok.Builder;
+import lombok.Cleanup;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 
 @Getter
 @NoArgsConstructor
 @Embeddable
 public class Address {
+    @Column(length = 10)
     private String city;
+    @Column(length = 20)
     private String street;
+    @Column(length = 5)
     private String zipCode;
 
     @Builder
@@ -23,16 +26,20 @@ public class Address {
         this.zipCode = zipCode;
     }
 
+    public String fullAddress() {
+        return getCity() + " " + getStreet() + " " + getZipCode();
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Address address = (Address) o;
-        return Objects.equals(city, address.city) && Objects.equals(street, address.street) && Objects.equals(zipCode, address.zipCode);
+        return Objects.equals(getCity(), address.getCity()) && Objects.equals(getStreet(), address.getStreet()) && Objects.equals(getZipCode(), address.getZipCode());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(city, street, zipCode);
+        return Objects.hash(getCity(), getStreet(), getZipCode());
     }
 }
--- src/main/java/io/ggammu/study/jpa/Delivery.java
+++ src/main/java/io/ggammu/study/jpa/Delivery.java
@@ -1,6 +1,7 @@
 package io.ggammu.study.jpa;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -18,9 +19,9 @@ public class Delivery {
     @Column(name = "DELIVERY_ID", nullable = false)
     private Long id;
 
-    private String city;
-    private String street;
-    private String zipcode;
+    @Embedded
+    private Address address;
+
     private DeliveryStatus status;
 
     @OneToOne(mappedBy = "delivery", fetch =  FetchType.LAZY)
