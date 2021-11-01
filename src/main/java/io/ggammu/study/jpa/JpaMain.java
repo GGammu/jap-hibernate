b23e87f3a811db2b0d88f28619610d09dd3e458b
Young Hwang <ykcul02@gmail.com>
2021-11-01 22:29:45 +0900

    fix: jpql join fetch를 이용한 데이터 한번에 가져오기

--- src/main/java/io/ggammu/study/jpa/JpaMain.java
+++ src/main/java/io/ggammu/study/jpa/JpaMain.java
@@ -14,32 +14,46 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("A team");
-            em.persist(team);
+            Team team1 = new Team();
+            team1.setName("팀A");
+            em.persist(team1);
 
-            Member member = new Member();
-            member.setUsername("member 1");
-            member.setAge(10);
-            member.setType(MemberType.ADMIN);
-            member.setTeam(team);
-            em.persist(member);
+            Team team2 = new Team();
+            team2.setName("팀B");
+            em.persist(team2);
 
             Member member1 = new Member();
-            member1.setUsername("member 2");
-            member1.setAge(20);
+            member1.setUsername("회원1");
+            member1.setAge(10);
             member1.setType(MemberType.ADMIN);
-            member1.setTeam(team);
+            member1.setTeam(team1);
             em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setAge(20);
+            member2.setType(MemberType.ADMIN);
+            member2.setTeam(team1);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원2");
+            member3.setAge(20);
+            member3.setType(MemberType.ADMIN);
+            member3.setTeam(team2);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select m.username from Team t join t.members m";
-            List<String> resultList = em.createQuery(query, String.class)
+            String query = "select m from Member m join fetch m.team";
+            List<Member> resultList = em.createQuery(query, Member.class)
                             .getResultList();
 
-            resultList.forEach(m -> System.out.println(m.toString()));
+            resultList.forEach(m -> {
+                System.out.println(m.getTeam().getName());
+                System.out.println(m.getUsername());
+            });
 
             tx.commit();
         } catch (Exception e) {
